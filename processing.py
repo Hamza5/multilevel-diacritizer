436c9acfe4448fd864b0a62a1437eb9b37b8e408
Hamza Abbad <hamza.abbad@gmail.com>
2019-10-06 21:03:34 +0800

    Added some required diacritics processing functions.

--- processing.py
+++ processing.py
@@ -1,4 +1,5 @@
 import re
+from typing import Iterable
 # The prefixes and suffixes that should be separated from the stem before converting it to a pattern
 SEPARATED_PREFIXES = {'ل', 'ك', 'ف', 'ت', 'ب', 'ن', 'ال', 'لل'}
 SEPARATED_PREFIXES.update({x + 'ال' for x in {'ك', 'ف', 'و', 'ب'}})
@@ -17,8 +18,13 @@ DIACRITICS = set(chr(code) for code in range(0x064B, 0x0653))
 DIACRITICS_PATTERN = re.compile('['+''.join(DIACRITICS)+']')
 
 
-def separate_affixes(u_word):
-    assert isinstance(u_word, str)
+def separate_affixes(u_word: str) -> (str, str, str):
+    """
+    Separate some affixes from the stem of an undiacritized word and return all the three parts.
+    :param u_word: str, an undiacritized word.
+    :return: (str, str, str), prefix, stem and suffix.
+    """
+    assert isinstance(u_word, str) and set(u_word).intersection(DIACRITICS) == set()
     possible_prefixes = sorted([s for s in SEPARATED_PREFIXES if u_word.startswith(s)], key=len, reverse=True)
     possible_suffixes = sorted([s for s in SEPARATED_SUFFIXES if u_word.endswith(s)], key=len, reverse=True)
     possible_prefixes.append('')
@@ -28,16 +34,61 @@ def separate_affixes(u_word):
         for s in possible_suffixes:
             if len(u_word) - (len(p) + len(s)) >= MIN_STEM_LEN:
                 acceptable_affixes.append((p, s))
+    if len(acceptable_affixes) == 0:
+        acceptable_affixes = [('', '')]
     prefix, suffix = max(acceptable_affixes, key=lambda x: len(x[0]) + len(x[1]))
     return prefix, u_word[len(prefix):len(u_word)-len(suffix)], suffix
 
 
-def convert_to_pattern(u_word) -> str:
-    assert isinstance(u_word, str)
-    prefix, stem, suffix = separate_affixes(u_word)
+def clear_diacritics(d_text: str) -> str:
+    """
+    Remove all standard diacritics from the text.
+    :param d_text: str, the diacritized text.
+    :return: str, the text undiacritized.
+    """
+    return DIACRITICS_PATTERN.sub('', d_text)
+
+
+def extract_diacritics(d_text: str) -> list:
+    """
+    Return the diacritics from the text while keeping their original positions including the Shaddah marks.
+    :param d_text: str, the diacritized text.
+    :return: list, the diacritics. Positions with double diacritics have a tuple as elements.
+    """
+    assert isinstance(d_text, str)
+    diacritics = []
+    for i in range(1, len(d_text)):
+        if d_text[i] in DIACRITICS:
+            if d_text[i - 1] == 'ّ':
+                diacritics[-1] = (d_text[i - 1], d_text[i])
+            else:
+                diacritics.append(d_text[i])
+        elif d_text[i - 1] not in DIACRITICS:
+            diacritics.append('')
+    if d_text[-1] not in DIACRITICS:
+        diacritics.append('')
+    return diacritics
+
+
+def merge_diacritics(u_text: str, diacritics: Iterable) -> str:
+    """
+    Return the diacritized text resulted from merging the text with the diacritics extracted by separate_diacritics.
+    :param u_text: str, the undiacritized text.
+    :param diacritics: list of str or tuple, the diacritics. Positions with double diacritics have a tuple as elements.
+    :return: str, the diacritized text.
+    """
+    assert isinstance(u_text, str)
+    assert isinstance(diacritics, Iterable)
+    return ''.join([l+''.join(d) for l, d in zip(u_text, diacritics)])
+
+
+def convert_to_pattern(word: str) -> str:
+    """
+    Return the corresponding pattern of a given word.
+    :param word: str, the word to convert.
+    :return: str, the pattern of the word.
+    """
+    assert isinstance(word, str)
+    prefix, stem, suffix = separate_affixes(clear_diacritics(word))
     stem = ORDINARY_ARABIC_LETTERS_PATTERN.sub('ح', stem)
-    return prefix + stem + suffix
-
-
-def clear_diacritics(d_sentence):
-    return DIACRITICS_PATTERN.sub('', d_sentence)
+    return merge_diacritics(prefix + stem + suffix, extract_diacritics(word))
