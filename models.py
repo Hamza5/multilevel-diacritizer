131d7686dc5dfeca9b3558d1ed26bd98e28318f7
Hamza Abbad <hamza.abbad@gmail.com>
2019-10-12 14:35:19 +0800

    Adjusted the starting probabilities of states in BigramHMMPatternDiacritizer.

--- models.py
+++ models.py
@@ -39,6 +39,11 @@ class MostFrequentPatternDiacritizer:
         self.word_diacritization.update({word: FakeState(name=diac) for word, diac in words_top_diac.items()})
 
     def predict(self, undiacritized_words_sequence):
+        """
+        Predict the sequence of diacritized words corresponding to the given undiacritized sequence.
+        :param undiacritized_words_sequence: list of undiacritized words of a sentence.
+        :return: list of diacritized words.
+        """
         assert isinstance(undiacritized_words_sequence, list) and all(isinstance(w, str) for w in
                                                                       undiacritized_words_sequence)
         return [merge_diacritics(u_w, extract_diacritics(self.word_diacritization[u_w].name))
@@ -63,7 +68,7 @@ class BigramHMMPatternDiacritizer:
         assert 0 < epsilon < 1
         d_words = []
         d_patterns_bigrams = []
-        self.u_patterns = set()
+        self.u_patterns = set()  # Needed for checking the out-of-vocabulary patterns in predict function.
         print('Extracting the words and generating the required forms...')
         for d_words_sequence in diacritized_word_sequences:
             d_words.extend(d_words_sequence)
@@ -71,13 +76,14 @@ class BigramHMMPatternDiacritizer:
             for d_w1, d_w2 in zip(d_words_sequence[:-1], d_words_sequence[1:]):
                 d_patterns_bigrams.append((convert_to_pattern(d_w1), convert_to_pattern(d_w2)))
         print('Indexing...')
-        patterns_unigrams_counter = Counter([convert_to_pattern(d_w) for d_w in d_words])
-        patterns_bigrams_counter = Counter(d_patterns_bigrams)
-        d_pattern_u_pattern_counter = defaultdict(Counter)
+        patterns_unigrams_counter = Counter([convert_to_pattern(d_w) for d_w in d_words])  # d_pattern: count.
+        patterns_bigrams_counter = Counter(d_patterns_bigrams)  # (d_pattern1, d_pattern2): count.
+        d_pattern_u_pattern_counter = defaultdict(Counter)  # (d_pattern, u_pattern): count.
         for d_word in d_words:
             d_pattern_u_pattern_counter[convert_to_pattern(d_word)][convert_to_pattern(clear_diacritics(d_word))] += 1
-        states = []
-        distributions = []
+        patterns_start_counter = Counter([s[0] for s in diacritized_word_sequences])
+        states = []  # Patterns.
+        distributions = []  # Emissions probabilities.
         print('Calculating the probabilities...')
         for pattern, u_pattern_count in d_pattern_u_pattern_counter.items():
             u_patterns_emissions = {UNKNOWN: epsilon}
@@ -86,21 +92,29 @@ class BigramHMMPatternDiacritizer:
             emissions_distribution = DiscreteDistribution(u_patterns_emissions)
             states.append(pattern)
             distributions.append(emissions_distribution)
-        transitions = np.ones((len(states), len(states))) * epsilon
+        transitions = np.ones((len(states), len(states))) * epsilon  # Initialize with a small strictly positive number.
         for i, d_pattern1 in enumerate(states):
             for j, (d_pattern2, distribution) in enumerate(zip(states, distributions)):
                 if (d_pattern1, d_pattern2) in patterns_bigrams_counter.keys():
                     transitions[i, j] += patterns_bigrams_counter[d_pattern1, d_pattern2] /\
-                                         patterns_unigrams_counter[d_pattern1]
-        transitions /= np.sum(transitions, axis=-1, keepdims=True)
-        transitions -= epsilon / transitions.shape[0]
+                                         patterns_unigrams_counter[d_pattern1]  # Add the true probability of the pair.
+        transitions /= np.sum(transitions, axis=-1, keepdims=True)  # Normalize everything.
+        transitions -= epsilon / transitions.shape[0]  # Remove the probabilities of the end state.
         end_probs = (1 - np.sum(transitions, axis=-1, keepdims=True)).flatten()
-        start_probs = np.ones(transitions.shape[0]) / transitions.shape[0]
+        start_probs = np.ones(transitions.shape[0]) * epsilon
+        for i, state in enumerate(states):
+            start_probs[i] += patterns_start_counter[state] / sum(patterns_start_counter.values())
+        start_probs /= np.sum(start_probs)
         print('Building the HMM...')
         self.model = HiddenMarkovModel.from_matrix(transitions, distributions, start_probs, end_probs, states,
-                                                   self.__class__.__name__, merge=None, verbose=True)
+                                                   self.__class__.__name__, merge='None')
 
     def predict(self, undiacritized_words_sequence):
+        """
+        Predict the sequence of diacritized words corresponding to the given undiacritized sequence.
+        :param undiacritized_words_sequence: list of undiacritized words of a sentence.
+        :return: list of diacritized words.
+        """
         assert isinstance(undiacritized_words_sequence, list) and all(isinstance(w, str) for w in
                                                                       undiacritized_words_sequence)
         sequence = []
