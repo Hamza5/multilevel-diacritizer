9fc0ede95fb6091cdf1e010e4468b392bce25484
Hamza Abbad <hamza.abbad@gmail.com>
2021-05-09 14:45:48 +0100

    Added test_train_dataset

--- tests/tests.py
+++ tests/tests.py
@@ -2,11 +2,14 @@ import unittest
 from pathlib import Path
 from argparse import Namespace
 
-from multilevel_diacritizer.multi_level_diacritizer import get_loaded_model
+import tensorflow as tf
+import numpy as np
+
+from multilevel_diacritizer.multi_level_diacritizer import get_loaded_model, get_dataset_from
 from multilevel_diacritizer.model import MultiLevelDiacritizer
 from multilevel_diacritizer.constants import (
     DEFAULT_WINDOW_SIZE, DEFAULT_LSTM_SIZE, DEFAULT_DROPOUT_RATE, DEFAULT_EMBEDDING_SIZE, DEFAULT_PARAMS_DIR,
-    DEFAULT_SLIDING_STEP,
+    DEFAULT_SLIDING_STEP, DEFAULT_BATCH_SIZE
 )
 
 
@@ -20,6 +23,16 @@ class ModelTestCase(unittest.TestCase):
         )
         self.assertIsInstance(model_path, Path)
         self.assertIsInstance(model, MultiLevelDiacritizer)
+
+    def test_train_dataset(self):
+        data = get_dataset_from(
+            [Path('tests/train_mini.txt')],
+            Namespace(batch_size=DEFAULT_BATCH_SIZE, window_size=DEFAULT_WINDOW_SIZE, sliding_step=DEFAULT_SLIDING_STEP)
+        )
+        self.assertIn('dataset', data.keys())
+        self.assertIn('size', data.keys())
+        self.assertIsInstance(data['dataset'], tf.data.Dataset)
+        self.assertEqual(np.ndim(data['size']), 0)
 
 
 if __name__ == '__main__':
