4eb69cbff6f8334e153a2879e1bd903e757d9afd
Hamza Abbad <hamza.abbad@gmail.com>
2021-05-13 18:53:03 +0100

    Added test_diacritization.

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -1,36 +1,35 @@
 import unittest
-import sys
 from io import StringIO
 from contextlib import redirect_stdout
 from logging import getLogger, StreamHandler
 
-from multilevel_diacritizer.multi_level_diacritizer import train_command, train_parser
+from multilevel_diacritizer.multi_level_diacritizer import (
+    train_command, train_parser, diacritization_command, diacritization_parser
+
+)
+from multilevel_diacritizer.constants import DIACRITICS_PATTERN
 
 
 class CommandsTestCase(unittest.TestCase):
-
-    @classmethod
-    def setUpClass(cls):
-        cls.train_model_args = train_parser.parse_args(['-t', 'tests/train_mini.txt', '-v', 'tests/train_mini.txt',
-                                                        '-e', '2', '-b', '256'])
 
     def setUp(self):
         self.stdout = StringIO()
         self.logging_stream = StringIO()
         getLogger('multilevel_diacritizer.multi_level_diacritizer').addHandler(StreamHandler(self.logging_stream))
-        # Back-up the existing files in PARAMS_DIR.
-        for file_path in self.train_model_args.params_dir.glob('*'):
-            file_path.rename(file_path.with_suffix(file_path.suffix+'~'))
 
     def test_train(self):
+        train_model_args = train_parser.parse_args(['-t', 'tests/train_mini.txt', '-v', 'tests/train_mini.txt',
+                                                    '-e', '2', '-b', '256'])
+        # Back-up the existing files in PARAMS_DIR.
+        for file_path in train_model_args.params_dir.glob('*'):
+            file_path.rename(file_path.with_suffix(file_path.suffix + '~'))
         with redirect_stdout(self.stdout):
-            train_command(self.train_model_args)
+            train_command(train_model_args)
         self.assertIn('val_loss', self.stdout.getvalue())
         self.assertIn('Training finished', self.logging_stream.getvalue())
-
-    def tearDown(self):
+        self.assertIn('Random weights', self.logging_stream.getvalue())
         # Remove the generated files and restore the original ones in PARAMS_DIR.
-        for file_path in self.train_model_args.params_dir.iterdir():
+        for file_path in train_model_args.params_dir.iterdir():
             if file_path.suffix.endswith('~'):
                 destination_path = file_path.with_suffix(file_path.suffix.rstrip('~'))
                 if destination_path.exists():
@@ -39,10 +38,20 @@ class CommandsTestCase(unittest.TestCase):
             else:
                 file_path.unlink()
 
-    @classmethod
-    def tearDownClass(cls):
-        sys.stdout = sys.__stdout__
-        sys.stderr = sys.__stderr__
+    def test_diacritization(self):
+        args = diacritization_parser.parse_args([])
+        with open('tests/train_mini.txt', encoding='UTF-8') as data_file:
+            u_text = DIACRITICS_PATTERN.sub('', ''.join(data_file.readlines()[:50]))  # Take the first 50 lines.
+        args.file = StringIO(u_text)
+        args.out_file = StringIO()
+        _close = args.out_file.close
+        args.out_file.close = lambda: None  # Prevent closing the stream by the command
+        diacritization_command(args)
+        self.assertIn('Done', self.logging_stream.getvalue())
+        self.assertIn('Model weights loaded', self.logging_stream.getvalue())
+        self.assertNotEqual(args.out_file.getvalue(), u_text)
+        self.assertEqual(DIACRITICS_PATTERN.sub('', args.out_file.getvalue()), u_text)
+        _close()
 
 
 if __name__ == '__main__':
