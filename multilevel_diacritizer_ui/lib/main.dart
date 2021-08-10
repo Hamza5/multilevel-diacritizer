9a56d583245c135ef037640411db56ac11c6391f
Hamza Abbad <hamza.abbad@gmail.com>
2021-08-10 18:53:32 +0100

    Added the information card.

--- multilevel_diacritizer_ui/lib/main.dart
+++ multilevel_diacritizer_ui/lib/main.dart
@@ -32,7 +32,17 @@ class MultilevelDiacritizerFrontEnd extends StatelessWidget {
         appBar: AppBar(
           title: Text('Multilevel diacritizer'),
         ),
-        body: DiacritizationForm(),
+        body: Container(
+          padding: const EdgeInsets.only(top: 20, left: 20, right: 20),
+          child: ListView(
+              children: [
+                DiacritizationForm(),
+                const SizedBox(height: 20),
+                const InformationArea(),
+                const SizedBox(height: 20,)
+              ]
+          ),
+        ),
       ),
     );
   }
@@ -104,134 +114,225 @@ class _DiacritizationFormState extends State<DiacritizationForm> {
   Widget build(BuildContext context) {
     return Form(
       key: _formKey,
-      child: Container(
-        padding: EdgeInsets.all(15),
-        child: ListView(
-          children: [
-            Row(
-              children: <Widget>[
-                Expanded(
-                  child: Stack(
-                    children: [
-                      TextFormField(
-                        readOnly: !_enableSubmit,
-                        enabled: _enableSubmit,
-                        textDirection: TextDirection.rtl,
-                        style: TextStyle(
-                          fontSize:
-                              Theme.of(context).textTheme.headline6?.fontSize,
-                        ),
-                        minLines: 10,
-                        maxLines: 100,
-                        decoration: InputDecoration(
-                          labelText: 'The Arabic text to diacritize',
-                          labelStyle: Theme.of(context)
-                              .textTheme
-                              .headline6
-                              ?.copyWith(color: Colors.blue),
-                          border: OutlineInputBorder(),
-                        ),
-                        controller: _textEditingController,
-                        autofocus: true,
+      child: Column(
+        children: [
+          Row(
+            children: <Widget>[
+              Expanded(
+                child: Stack(
+                  children: [
+                    TextFormField(
+                      readOnly: !_enableSubmit,
+                      enabled: _enableSubmit,
+                      textDirection: TextDirection.rtl,
+                      style: TextStyle(
+                        fontSize:
+                            Theme.of(context).textTheme.headline6?.fontSize,
                       ),
-                      Container(
-                        padding: EdgeInsets.all(1),
-                        child: Visibility(
-                          visible: !_enableSubmit,
-                          child: ClipRRect(
-                            child: LinearProgressIndicator(
-                              minHeight: 5,
-                              valueColor:
-                                  AlwaysStoppedAnimation(Colors.blueAccent),
-                            ),
-                            borderRadius: BorderRadius.circular(2),
+                      minLines: 10,
+                      maxLines: 100,
+                      decoration: InputDecoration(
+                        labelText: 'The Arabic text to diacritize',
+                        labelStyle: Theme.of(context)
+                            .textTheme
+                            .headline6
+                            ?.copyWith(color: Colors.blue),
+                        border: OutlineInputBorder(),
+                      ),
+                      controller: _textEditingController,
+                      autofocus: true,
+                    ),
+                    Container(
+                      padding: EdgeInsets.all(1),
+                      child: Visibility(
+                        visible: !_enableSubmit,
+                        child: ClipRRect(
+                          child: LinearProgressIndicator(
+                            minHeight: 5,
+                            valueColor:
+                                AlwaysStoppedAnimation(Colors.blueAccent),
                           ),
+                          borderRadius: BorderRadius.circular(2),
                         ),
                       ),
-                    ],
-                    alignment: Alignment.bottomCenter,
-                  ),
-                ),
-                Column(
-                  children: <Widget>[
-                    IconButton(
-                      icon: Icon(Icons.copy),
-                      onPressed: _enableSubmit
-                          ? () =>
-                              FlutterClipboard.copy(_textEditingController.text)
-                                  .then(
-                                      (value) =>
-                                          _showSnackBar(
-                                              'Text copied to clipboard!',
-                                              Icons.info,
-                                              Colors.blueAccent))
-                                  .catchError((error) => _showSnackBar(
-                                      error.toString(),
-                                      Icons.warning,
-                                      Theme.of(context).errorColor))
-                          : null,
-                      tooltip: 'Copy the text to the clipboard',
                     ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    IconButton(
-                      icon: Icon(Icons.paste),
-                      onPressed: _enableSubmit
-                          ? () => FlutterClipboard.paste().catchError((error) {
-                                _showSnackBar(error.toString(), Icons.warning,
-                                    Theme.of(context).errorColor);
-                                return '';
-                              }).then((value) {
-                                setState(
-                                    () => _textEditingController.text = value);
-                              })
-                          : null,
-                      tooltip: 'Paste the text from the clipboard',
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    IconButton(
-                      icon: Icon(Icons.clear),
-                      onPressed: _enableSubmit
-                          ? () {
-                              setState(() => _textEditingController.clear());
-                            }
-                          : null,
-                      tooltip: 'Clear the text',
-                    )
                   ],
+                  alignment: Alignment.bottomCenter,
                 ),
-              ],
-            ),
-            SizedBox(
-              height: 5,
-            ),
-            FittedBox(
-              fit: BoxFit.scaleDown,
-              child: ElevatedButton.icon(
-                onPressed: _enableSubmit ? submit : null,
-                label: Text(
-                  'Restore diacritics',
-                  textScaleFactor: 1.5,
-                ),
-                icon: TweenAnimationBuilder(
-                  child: Icon(
-                    Icons.settings_backup_restore,
-                    color: Theme.of(context).scaffoldBackgroundColor,
+              ),
+              Column(
+                children: <Widget>[
+                  IconButton(
+                    icon: Icon(Icons.copy),
+                    onPressed: _enableSubmit
+                        ? () =>
+                            FlutterClipboard.copy(_textEditingController.text)
+                                .then(
+                                    (value) =>
+                                        _showSnackBar(
+                                            'Text copied to clipboard!',
+                                            Icons.info,
+                                            Colors.blueAccent))
+                                .catchError((error) => _showSnackBar(
+                                    error.toString(),
+                                    Icons.warning,
+                                    Theme.of(context).errorColor))
+                        : null,
+                    tooltip: 'Copy the text to the clipboard',
                   ),
-                  duration: Duration(milliseconds: 500),
-                  tween: Tween<double>(begin: 0, end: _angle),
-                  builder: (BuildContext context, double angle, Widget? child) {
-                    return Transform.rotate(
-                      angle: angle,
-                      child: child,
-                    );
-                  },
-                  onEnd: () => setState(() {
-                    if (!_enableSubmit) _angle -= pi;
-                  }),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.paste),
+                    onPressed: _enableSubmit
+                        ? () => FlutterClipboard.paste().catchError((error) {
+                              _showSnackBar(error.toString(), Icons.warning,
+                                  Theme.of(context).errorColor);
+                              return '';
+                            }).then((value) {
+                              setState(
+                                  () => _textEditingController.text = value);
+                            })
+                        : null,
+                    tooltip: 'Paste the text from the clipboard',
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.clear),
+                    onPressed: _enableSubmit
+                        ? () {
+                            setState(() => _textEditingController.clear());
+                          }
+                        : null,
+                    tooltip: 'Clear the text',
+                  )
+                ],
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 5,
+          ),
+          FittedBox(
+            fit: BoxFit.scaleDown,
+            child: ElevatedButton.icon(
+              onPressed: _enableSubmit ? submit : null,
+              label: Text(
+                'Restore diacritics',
+                textScaleFactor: 1.5,
+              ),
+              icon: TweenAnimationBuilder(
+                child: Icon(
+                  Icons.settings_backup_restore,
+                  color: Theme.of(context).scaffoldBackgroundColor,
+                ),
+                duration: Duration(milliseconds: 500),
+                tween: Tween<double>(begin: 0, end: _angle),
+                builder: (BuildContext context, double angle, Widget? child) {
+                  return Transform.rotate(
+                    angle: angle,
+                    child: child,
+                  );
+                },
+                onEnd: () => setState(() {
+                  if (!_enableSubmit) _angle -= pi;
+                }),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class InformationArea extends StatelessWidget {
+  const InformationArea({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      shadowColor: Theme.of(context).primaryColorDark,
+      elevation: 2,
+      color: Theme.of(context).primaryColorLight,
+      child: DefaultTextStyle(
+        style: TextStyle(
+          color: Theme.of(context).primaryColorDark,
+          fontSize: 16,
+        ),
+        child: Column(
+          children: [
+            Card(
+              color: Theme.of(context).primaryColor,
+              margin: const EdgeInsets.only(bottom: 5),
+              child: Padding(
+                padding: EdgeInsets.all(5),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.info,
+                      color: Theme.of(context).scaffoldBackgroundColor,
+                    ),
+                    SizedBox(width: 10,),
+                    Text(
+                      'Information',
+                      style: TextStyle(
+                        color: Theme.of(context).scaffoldBackgroundColor,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                      ),
+                    ),
+                  ]
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: Text.rich(
+                TextSpan(
+                  text: 'This web application allow the user to predict the'
+                      ' diacritics of a diacritics-free Arabic text. The text '
+                      'can be simply inserted in the text field above then sent'
+                      ' to the server by clicking the ',
+                  children: [
+                    TextSpan(
+                      text: 'Restore diacritcs',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    TextSpan(
+                      text: ' button. When the server response arrive, the '
+                          'diacritized text will be used to overwrite the '
+                          'original text.',
+                    )
+                  ]
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 10, right: 10, bottom: 10),
+              child: Text.rich(
+                TextSpan(
+                  text: 'You can use this application as an API by just sending'
+                      ' an HTTP POST request to the same URL that this '
+                      'application is served from with the Arabic text as the '
+                      'body (',
+                  children: [
+                    TextSpan(
+                      text: 'Content-Type: text/plain',
+                      style: TextStyle(
+                        backgroundColor: Theme.of(context).canvasColor,
+                      ),
+                    ),
+                    TextSpan(
+                      text: '). The response will contain the diacritized text.'
+                    )
+                  ]
                 ),
               ),
             ),
